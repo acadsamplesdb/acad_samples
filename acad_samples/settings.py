0f5286a7c5cba551fc73ddc27a7fce74f5a736a8
Craig Patten <craigpatten@users.noreply.github.com>
2015-12-20 01:11:36 +1030

    +STATIC_ROOT

--- acad_samples/settings.py
+++ acad_samples/settings.py
@@ -101,6 +101,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/data/samples/static'
 
 LOGIN_URL = '/login/'
 LOGIN_EXEMPT_URLS = (
