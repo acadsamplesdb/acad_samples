d05a7fdb9f7b65441708e8a194cb311c9cbe5f77
Jianfeng Li <jianfeng.li@ersa.edu.au>
2016-02-03 16:27:02 +1030

    Add file icons to attached files on detail page

--- samples/templatetags/floatcutzeroes.py
+++ samples/templatetags/floatcutzeroes.py
@@ -4,3 +4,25 @@ register = template.Library()
 @register.filter
 def floatcutzeroes(value):
     return float(value)
+
+@register.simple_tag
+def filetype_icon(file_name, **kwargs):
+    icon_name = 'fa-file-o'
+    size = kwargs['size'] if 'size' in kwargs else '2em'
+    import os
+    _, ext = os.path.splitext(file_name)
+    if len(ext) > 2:
+        ext = ext[1:]
+        icons = {'pdf': 'pdf',
+                 'jpg': 'picture',
+                 'png': 'picture',
+                 'txt': 'text',
+                 'docx': 'word',
+                 'xlsx': 'excel',
+                 'zip': 'archive',
+                 'gz': 'archive',
+                }
+        if ext in icons.keys():
+            icon_name = 'fa-file-%s-o' % icons[ext]
+
+    return '<i class="fa %s" style="margin-right:10px;font-size:%s;"></i>' % (icon_name, size)
