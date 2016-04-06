db6291b65ed59af7f7b9f9a4c0c85d8ced0b33a4
Jianfeng Li <jianfeng.li@ersa.edu.au>
2016-04-06 10:35:17 +0930

    Display thumbnails of picture #6

--- samples/templatetags/floatcutzeroes.py
+++ samples/templatetags/floatcutzeroes.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse
 from django import template
 register = template.Library()
 
@@ -5,24 +6,33 @@ register = template.Library()
 def floatcutzeroes(value):
     return float(value)
 
+def _scale_image(file_id, **kwargs):
+    # Not really scale image but use css to pretend for the time being
+    img_url = reverse('download_file', args=(file_id,))
+    width = kwargs['width'] if 'width' in kwargs else '200'
+    height = kwargs['height'] if 'height' in kwargs else '200'
+    padleft = kwargs['padleft'] if 'padleft' in kwargs else '5'
+    return '<img src="%s" class="img-thumbnail" width="%s" height="%s" style="margin-right:%spx">' % (img_url, width, height, padleft)
+
 @register.simple_tag
-def filetype_icon(file_name, **kwargs):
+def filetype_icon(file_name, file_id, **kwargs):
     icon_name = 'fa-file-o'
     size = kwargs['size'] if 'size' in kwargs else '2em'
     import os
     _, ext = os.path.splitext(file_name)
+    ext = ext.lower()
     if len(ext) > 2:
         ext = ext[1:]
         icons = {'pdf': 'pdf',
-                 'jpg': 'picture',
-                 'png': 'picture',
                  'txt': 'text',
                  'docx': 'word',
                  'xlsx': 'excel',
                  'zip': 'archive',
                  'gz': 'archive',
                 }
-        if ext in icons.keys():
+        if ext in ['jpg', 'jpeg', 'png']:
+            return _scale_image(file_id, **kwargs)
+        elif ext in icons.keys():
             icon_name = 'fa-file-%s-o' % icons[ext]
 
     return '<i class="fa %s" style="margin-right:10px;font-size:%s;"></i>' % (icon_name, size)
